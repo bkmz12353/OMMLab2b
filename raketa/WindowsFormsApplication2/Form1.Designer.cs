9e24bbb05b43b9742eb0bb7fc9c1950bfc7ccba7
Ilya <61599600+bkmz12353@users.noreply.github.com>
2020-05-06 12:25:30 +0300

    Add files via upload

---
+++ raketa/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1312, 632);
+            this.panel.TabIndex = 0;
+            this.panel.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.chart1.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.TileFlipXY;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            this.chart1.BorderSkin.BorderWidth = 2;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1288, 620);
+            this.chart1.TabIndex = 100;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 632);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Моделирование полёта ракеты ";
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox panel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ raketa/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,149 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel;
+
+using System.Data;
+
+using System.Drawing;
+
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+            double dt = 0.01;
+            double M = 6 * Math.Pow(10, 24);
+            double x = 0;
+            double y= 6.4 * Math.Pow(10, 6);
+        double graviconst = 6.672 * Math.Pow(10,-11);
+        
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series.Clear();
+
+            chart1.Series.Add("Ракета со скоростью 7500 м/c");
+
+            chart1.Series.Add("Ракета со скоростью 7923 м/c");
+
+            chart1.Series.Add("Ракета2 со скоростью 10000 м/c");
+            chart1.Series.Add("Ракета 3со скоростью 11206 м/c");
+            Axis ax = new Axis();
+            ax.Title = "X";
+            chart1.ChartAreas[0].AxisX = ax;
+            Axis ay = new Axis();
+            ay.Title = "Y";
+            chart1.ChartAreas[0].AxisY = ay;
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            
+            chart1.Series[0].BorderWidth = 4;
+
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+
+            chart1.Series[1].BorderWidth = 3;
+
+            chart1.Series[2].ChartType = SeriesChartType.Line;
+
+            chart1.Series[2].BorderWidth = 2;
+            chart1.Series[2].ChartType = SeriesChartType.Line;
+
+            chart1.Series[3].BorderWidth = 1;
+            chart1.Series[3].ChartType = SeriesChartType.Line;
+
+            Rocket v1 = new Rocket();
+            v1.velocity = 7500;
+            Rocket v2 = new Rocket();
+            v2.velocity = 7923;
+            Rocket v3 = new Rocket();
+            v3.velocity = 10000;
+            Rocket v4 = new Rocket();
+            v4.velocity = 11206;
+            GetData(v1.velocity);
+            GetData(v2.velocity);
+            GetData(v3.velocity);
+            GetData(v4.velocity);
+            
+
+        }
+
+        public void GetData(double velocity)
+        {
+            string FileName = "";
+            if (velocity == 7500) { FileName = "Rocket1.txt"; }
+            else if (velocity == 7923) { FileName = "Rocket2.txt"; }
+            else if (velocity == 10000) { FileName = "Rocket3.txt"; }
+            else if (velocity == 11206) { FileName = "Rocket4.txt"; }
+            StreamWriter writedata = new StreamWriter(FileName);
+            double coeff = graviconst * dt * M;
+            double xx = x;
+            double yy = y;
+            int datax = 0;
+            string data="";
+            double n = 100;
+            double xt;
+            double yt;
+            double vxt=velocity;
+            double vyt=velocity/1000;
+            double r;
+            double[,] datamass=new double[Convert.ToInt32(n/dt+1),2];
+            datamass[0, 0] = x;
+            datamass[0, 1] = y;
+            for (double t=0;t<=n;t+=dt)
+            {
+                datax++;
+                xt = xx + vxt * dt;
+                yt = yy + vyt * dt;
+                //try { 
+                datamass[datax, 0] = yt;
+                datamass[datax, 1] = xt;
+                //}
+                //catch { DrawGraph(datamass, n, velocity); }
+                data = data + "\n" + Convert.ToString(xt) + "   " + Convert.ToString(yt);
+
+                r = Math.Sqrt(xt * xt + yt * yt);
+                vxt = vxt - (coeff*xt/(r*r*r));
+                vyt = vyt + (coeff* yt/ (r * r * r));
+                xx = xt;
+                yy = yt;
+            }
+            writedata.Write(data);
+            writedata.Close();
+            DrawGraph(datamass,n,velocity);
+        }
+        public void DrawGraph(double[,] mass, double n, double velocity)
+        {
+
+            int z = 0;
+            if (velocity == 7500) { z = 0; }
+            else if (velocity == 7923) { z = 1; }
+            else if (velocity == 10000) { z = 2; }
+            else if (velocity == 11206) { z = 3; }
+            for (int i = 1; i < mass.GetLength(0); i++)
+
+            {
+                chart1.Series[z].Points.AddXY(mass[i,0], mass[i, 1]);
+            }
+            
+        }
+    }
+    class Rocket
+    {
+        private double _velocity;
+        public double velocity
+        { 
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+    }
+        
+}
+
